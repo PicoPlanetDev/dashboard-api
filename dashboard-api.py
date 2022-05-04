7a4a81dbb685b96abd3cccbaf4a74b6e71ecda83
PicoPlanetDev <picoplanetdev@gmail.com>
2022-05-04 06:22:52 -0400

    Added wake functionality

--- dashboard-api.py
+++ dashboard-api.py
@@ -22,14 +22,31 @@ def get_student():
 
 @app.route('/grade', methods=['GET', 'POST'])
 def grade():
-    term_name = "S2"
-
     content = request.get_json()
+    response = determineRequestPurpose(content)
+
+    return response, 200
+
+def determineRequestPurpose(content):
+    handler = content['handler']['name']
+    if handler == "get_grade":
+        return get_grade(content)
+    else:
+        return wake()
+
+def wake():
+    responseText = "I'm ready to help check your grades!"
+    jsonResponse = json.dumps({"prompt": {"firstSimple": {"speech": responseText,"text": responseText}}})
+
+    return jsonResponse
+
+def get_grade(content):
     section_name = content['intent']['params']['class']['resolved']
 
     student = get_student()
     parser = studentParser.StudentParser(student)
 
+    term_name = "S2"
     grade = parser.getGrade(parser.convertNameAndSection(section_name), parser.convertTermNameToIds(term_name))
 
     percent = str(int(grade[1]))
@@ -37,7 +54,7 @@ def grade():
     responseText = "You have a {} percent in {}.".format(percent, section_name)
     jsonResponse = json.dumps({"prompt": {"firstSimple": {"speech": responseText,"text": responseText}}})
 
-    return jsonResponse, 200
+    return jsonResponse
 
 if __name__ == '__main__':
     app.run()
